fd60cd7d364356ca93e55e9f840ce64412146b2e
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-07-17 12:14:03 +0100

    Returning JSON content on TestAPI

--- src/main/java/br/com/fantonio/sigepi/service/api/TestAPI.java
+++ src/main/java/br/com/fantonio/sigepi/service/api/TestAPI.java
@@ -1,14 +1,37 @@
 package br.com.fantonio.sigepi.service.api;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/test")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TestAPI {
 
     @GET
-    public String helloWorld() {
-        return "Hellow World!";
+    public Response helloWorld() {
+
+        HelloObject message = new HelloObject("Hello World!!");
+
+        return Response.ok().entity(message).build();
     }
+
+    private class HelloObject {
+        private final String message;
+
+        public HelloObject(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+
+    }
+
 
 }
