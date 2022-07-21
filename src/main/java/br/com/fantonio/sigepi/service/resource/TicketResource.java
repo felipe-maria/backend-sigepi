48363f0aa6b9bdbb1bd1c4e422a5afd26f40b39e
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-07-21 09:16:12 +0100

    Added Openapi3 and create a very initial version of Ticketsapi

---
+++ src/main/java/br/com/fantonio/sigepi/service/resource/TicketResource.java
@@ -0,0 +1,24 @@
+package br.com.fantonio.sigepi.service.resource;
+
+import br.com.fantonio.sigepi.service.api.TicketsApi;
+import br.com.fantonio.sigepi.service.api.model.TicketTO;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketResource implements TicketsApi {
+
+    @Override
+    public Response findAllTickets() {
+        List<TicketTO> tos = new ArrayList<>();
+        TicketTO to = new TicketTO();
+        to.setId(1L);
+        tos.add(to);
+        to = new TicketTO();
+        to.setId(2L);
+        tos.add(to);
+
+        return Response.ok().entity(tos).build();
+    }
+}
