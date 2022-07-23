670f4099280be4aba26151289e6a2d2f691e9701
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-07-23 19:30:26 +0100

    Moved datasource configuration to docker-compose.yml

---
+++ src/main/java/br/com/fantonio/sigepi/service/domain/entity/TicketAnswer.java
@@ -0,0 +1,26 @@
+package br.com.fantonio.sigepi.service.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "t_ticket_answer")
+@Data
+public class TicketAnswer {
+
+    @Id
+    @GeneratedValue(generator = "s_ticket_answer")
+    private Long id;
+
+    private String answer;
+
+    @Column(name = "creation_date")
+    private LocalTime creationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "responsible_id")
+    private User responsible;
+
+}
