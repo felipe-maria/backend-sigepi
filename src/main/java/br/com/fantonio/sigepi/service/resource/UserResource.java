b8a351c3c5489ca4256fc4b984fb309bd1174c87
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-07-26 12:38:27 +0100

    Included hibernate validations but still not working

--- src/main/java/br/com/fantonio/sigepi/service/resource/UserResource.java
+++ src/main/java/br/com/fantonio/sigepi/service/resource/UserResource.java
@@ -1,7 +1,9 @@
 package br.com.fantonio.sigepi.service.resource;
 
 import br.com.fantonio.sigepi.service.api.UsersApi;
+import br.com.fantonio.sigepi.service.api.model.CreateUserITO;
 import br.com.fantonio.sigepi.service.api.model.UserTO;
+import br.com.fantonio.sigepi.service.business.CreateUser;
 import br.com.fantonio.sigepi.service.business.ListAllUsers;
 import br.com.fantonio.sigepi.service.domain.entity.User;
 import org.modelmapper.ModelMapper;
@@ -11,10 +13,28 @@ import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+
 public class UserResource implements UsersApi {
 
     @Inject
-    private ListAllUsers listAllUsers;
+    ListAllUsers listAllUsers;
+
+    @Inject
+    CreateUser createUser;
+
+    @Override
+    public Response createUser(CreateUserITO input) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        User entity = modelMapper.map(input, new TypeToken<User>() {}.getType());
+
+//        createUser.doIt(entity);
+
+        UserTO to = modelMapper.map(input, new TypeToken<UserTO>() {
+        }.getType());
+
+        return Response.ok().entity(to).build();
+    }
 
     @Override
     public Response listAllUsers() {
